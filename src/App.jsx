6062b81938a596bec70371244be4e625c1511057
Свят <@email.com>
2026-05-21 18:40:59 +0300

    ABOBA

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import './App.css'
+// Точний імпорт за твоєю структурою папок
+import Navbar from './component/Navbar/Navbar';
 
 function App() {
- 
-
   return (
-    <>
-     
-    </>
-  )
+        <Navbar />
+  );
 }
 
-export default App
+export default App;
